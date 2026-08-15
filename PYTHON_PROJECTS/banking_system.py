1cdcea2be2af0510f669b843745773594ee557eb
Pranav Sahu <pranavsahu688@gmail.com>
2026-08-15 19:23:43 +0530

    improve dashboard

--- PYTHON_PROJECTS/banking_system.py
+++ PYTHON_PROJECTS/banking_system.py
@@ -8,25 +8,33 @@ title = Label(
     text="PYTHON BANK",
     font=("Arial", 18, "bold")
 )
-
-# registration page
-def registration():
-    root = Tk()
-    pass
-
-
-def login():
-    root = Tk()
-    pass
-
+#main page
 def dashboard():
     
     root = Tk()
     root.geometry("300x400")
-    reg = Button(root,text="Create Account")
-    log = Button(root,text="Login")
+    reg = Button(root,text="Create Account",command=registration)
+    log = Button(root,text="Login",command=login)
     reg.place(x=100.,y=100,width=100,height=50)
     log.place(x=100.,y=200,width=100,height=50)
+
+# registration page
+def registration():
+    root = Tk()
+    root.geometry("300x500")
+    n = Label(root,text="Name").place(x=50,y=100,width=100,height=50)
+    i = Label(root,text="ID").place(x=50,y=170,width=100,height=50)
+    p = Label(root,text="Pass").place(x=50,y=250,width=100,height=50)
+    e = Label(root,text="Email").place(x=50,y=320,width=100,height=50)
+    t1 =Entry(root).place(x=120,y=100,width=170,height=50)
+    t2 =Entry(root).place(x=120,y=170,width=170,height=50)
+    t3 =Entry(root).place(x=120,y=250,width=170,height=50)
+    t4 =Entry(root).place(x=120,y=320,width=170,height=50)
+    s = Button(root,text="submit",command=dashboard).place(x=75,y=380,width=100,height=50)
+def login():
+    root = Tk()
+    pass
+
 
 main = Button(root,text="click to start",command=dashboard)
 main.place(x=100.,y=100,width=100,height=50)
